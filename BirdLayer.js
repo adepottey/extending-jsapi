e1944b136a8194acae1e3b8860478d9872ece446
Ashley DePottey <adepottey@esri.com>
2022-07-11 20:09:24 -0700

    Draw fancy images

--- BirdLayer.js
+++ BirdLayer.js
@@ -1,18 +1,37 @@
+const borderInPx = 8;
+
 export default (BaseLayerView2D, Layer) => {
   const CustomBirdLayerView2D = BaseLayerView2D.createSubclass({
     render: function ({ state, context }) {
-      this.layer.points.forEach((graphic, i) => {
+      this.layer.points.forEach((point, i) => {
         // get the screen coordinates for our point
-        var screenCoords = state.toScreen(
+        const screenCoords = state.toScreen(
           [0, 0],
-          graphic.geometry.x,
-          graphic.geometry.y
+          point.geometry.x,
+          point.geometry.y
         );
-        // draw image with upper-left corner at the point
-        context.drawImage(
-          this.layer.images[i],
+        const image = this.layer.images[i];
+        // background rectangle / border with upper-left corner at the point
+        context.fillStyle = "black";
+        context.fillRect(
           screenCoords[0],
-          screenCoords[1]
+          screenCoords[1],
+          image.width + 2 * borderInPx,
+          image.height + 2 * borderInPx
+        );
+        // draw image
+        context.drawImage(
+          image,
+          screenCoords[0] + borderInPx,
+          screenCoords[1] + borderInPx
+        );
+        // add text
+        context.fillStyle = "white";
+        context.font = "40px Arial";
+        context.fillText(
+          point.attributes.birdname,
+          screenCoords[0] + borderInPx,
+          screenCoords[1] + 40 + borderInPx
         );
       });
     },
